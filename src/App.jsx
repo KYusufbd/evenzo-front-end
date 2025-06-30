c6efae1c7b81a6d24fbd639befd93dcdf77922d7
Kazi Yusuf <yfaka001@gmail.com>
2025-07-01 05:52:21 +0600

    Added color themes tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ function App() {
   return (
     <div className='flex flex-col items-center justify-center gap-5 h-screen'>
       <h1 className="text-center font-semibold text-3xl">Assalamu alaikum!</h1>
-      <p className="text-center text-medium text-xl">Welcome to Evenzo!</p>
+      <p className="text-center text-medium text-xl text-neutral">Welcome to Evenzo!</p>
       <button className="btn btn-primary">Click me!</button>
+      <button className="btn btn-secondary">Click me!</button>
+      <button className="btn btn-accent">Click me!</button>
+      <button className="btn btn-neutral">Click me!</button>
     </div>
   );
 }
